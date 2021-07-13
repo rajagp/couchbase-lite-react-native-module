1f01f674ce0f97763eec0dc47beec3cccccd5b26
Linkitsoft <73120628+Linkitsoft@users.noreply.github.com>
2021-07-14 00:28:46 +0500

    Initial Commit
    Login page
    Profile Page
    Native Module (Android)
      Functions :
         Create and open database
         Fetch User Profile
         Save User Profile

---
+++ CBReactnativeApp/assets/components/CBNativemodule.js
@@ -0,0 +1,4 @@
+import { NativeModules } from 'react-native';
+module.exports = NativeModules.CouchbaseNativeModule;
+
+// Class to get Native Module this Exports the functions which can be used as Javascript code.
