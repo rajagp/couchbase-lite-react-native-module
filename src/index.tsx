1d41ed33ea8e1ee85f36ad57f0142f9064635a6f
Linkitsoft <73120628+Linkitsoft@users.noreply.github.com>
2021-07-17 01:38:51 +0500

    Revert

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type CbliteAndroidModuleType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { CbliteAndroidModule } = NativeModules;
-
-export default CbliteAndroidModule as CbliteAndroidModuleType;
