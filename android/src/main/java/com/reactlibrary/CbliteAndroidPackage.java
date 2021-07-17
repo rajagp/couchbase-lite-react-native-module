b0d99d67f149e24d5081469cf50d05e8131958cf
Linkitsoft <73120628+Linkitsoft@users.noreply.github.com>
2021-07-17 05:08:43 +0500

    Fix.
    Null pointer exception
    AAR package

--- android/src/main/java/com/reactlibrary/CbliteAndroidPackage.java
+++ android/src/main/java/com/reactlibrary/CbliteAndroidPackage.java
@@ -19,11 +19,6 @@ public class CbliteAndroidPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return null;
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
